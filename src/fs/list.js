3bd872245ba0c318c3f032acacb651df5ceaa1bd
Alexander <renyreny125tt@gmail.com>
2022-11-29 22:39:46 +0300

    feat: add function that prints all array of filenames from files folder

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import * as fs from 'node:fs';
+import { readdir } from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathFolder = path.join(__dirname, 'files');
+const errorText = 'FS operation failed';
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const existsFolder = fs.existsSync(pathFolder);
+
+        if(!existsFolder) {
+            throw new Error(errorText);
+        } else {
+            const files = await readdir(pathFolder);
+            console.info(files);
+        }
+    } catch (error) {
+        console.error(error); 
+    } 
 };
 
 await list();
