9cb2a3ee8aacfed36dcee8bb2bb826a4f1c40be1
Alexander <renyreny125tt@gmail.com>
2022-12-01 23:07:53 +0300

    feat: add function env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const processEnvObj = process.env;
+    const correctPrefix = 'RSS_';
+
+    const entriesProcessEnv = Object.entries(processEnvObj);
+
+    const arrCorrectVariables = entriesProcessEnv.filter(el => el[0].startsWith(correctPrefix, 0));
+    
+    const arrResults = arrCorrectVariables.map(el => `${el[0]}=${el[1]}`);
+
+    console.info(arrResults.join('; '));
 };
 
 parseEnv();
