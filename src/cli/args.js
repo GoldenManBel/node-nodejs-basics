7d8961e1ec6c2f350c08458b2244467fb344f4c9
Alexander <renyreny125tt@gmail.com>
2022-12-01 23:35:32 +0300

    feat: add function args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+    const arg = process.argv.slice(2);
+
+    const result = arg.map((el, index) => {
+        return index === arg.length - 1 ? el :
+               index % 2 === 0 ? `${el} is ` :
+                                   `${el}, ` ;
+    });
+
+    console.info(result.join(''));
 };
 
 parseArgs();
