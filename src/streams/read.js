9c5014436bc7a13ec1a1d0dda6901546036f1324
Alexander <renyreny125tt@gmail.com>
2022-12-02 12:19:54 +0300

    feat: add function read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import * as fs from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileRead = './files/fileToRead.txt';
+const fileReadPath = path.join(__dirname, fileRead);
+
+const readStream = fs.createReadStream(fileReadPath);
+const stdOut = process.stdout;
+
 const read = async () => {
-    // Write your code here 
+    try {
+        await pipeline(readStream, stdOut);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await read();
