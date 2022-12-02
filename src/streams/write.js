befe5724b231664112ae4306b91c175b988c2bc5
Alexander <renyreny125tt@gmail.com>
2022-12-02 12:23:53 +0300

    feat: add function write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import * as fs from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileWrite = './files/fileToWrite.txt';
+const fileWritePath = path.join(__dirname, fileWrite);
+
+const writeStream = fs.createWriteStream(fileWritePath);
+const stdIn = process.stdin;
+
 const write = async () => {
-    // Write your code here 
+    try {
+        await pipeline(stdIn, writeStream);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await write();
