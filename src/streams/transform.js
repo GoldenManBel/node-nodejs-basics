5b794cdc96bebb7e309a04dfa0a48d8763b32506
Alexander <renyreny125tt@gmail.com>
2022-12-02 13:25:41 +0300

    feat: add function transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
+const stdIn = process.stdin;
+const stdOut = process.stdout;
+
+const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+        callback(null, String(chunk).split('').reverse().join('').trim() + '\n');
+    }
+  });
+
 const transform = async () => {
-    // Write your code here 
+    try {
+        await pipeline(stdIn, transformStream, stdOut);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await transform();
