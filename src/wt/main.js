41ac637b1c955dc09151de670f23c5047d115641
Alexander <renyreny125tt@gmail.com>
2022-12-04 18:29:22 +0300

    feat: add function worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,73 @@
+import { Worker } from 'node:worker_threads';
+import os from 'os';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePathWorker = path.join(__dirname, './worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+    try {
+        let numBase = 10;
+        const numOfCpus = os.cpus().length;
+        const result = [];
+
+        for (let index = 0; index < numOfCpus; index++) {  
+            let argWorker = numBase;
+
+            if(numBase === 11 || numBase === 15) {
+                argWorker = 0;
+            } 
+            
+            const worker = new Worker(filePathWorker, {
+                workerData: {
+                    argWorker,
+                    index
+                }
+            });
+    
+            worker.on('message', (data) => {
+                result.push({
+                    status: 'resolved',
+                    data: data.result,
+                    index
+                });
+            });
+            worker.on('error', (error) => {
+                result.push({
+                    status: 'error',
+                    data: null,
+                    index
+                });
+            });
+            worker.on('exit', (code) => {       
+                if(result.length === numOfCpus) {
+                    result.sort((a, b) => {
+                        if (a.index > b.index) {
+                            return 1;
+                        }
+                        if (a.index < b.index) {
+                            return -1;
+                        }
+                        return 0;
+                    });  
+
+                    const resultOutput = result.map(el => {
+                        const { status, data } = el;
+                        return { status, data };
+                    });
+
+                    console.info(resultOutput);
+                }                
+            });
+
+            numBase++;  
+        };
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,9 +3,13 @@ import { workerData, parentPort } from 'node:worker_threads';
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    const mainThreadData = workerData;
-    const result = nthFibonacci(mainThreadData);
-    parentPort.postMessage(result);
+    const { argWorker, index } = workerData;
+    if(argWorker) {
+        const result = nthFibonacci(argWorker);
+        parentPort.postMessage({ result, index });
+    } else {
+        throw new Error('worker is error!');
+    }  
 };
 
 sendResult();
